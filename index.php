03a33a8eabf8dd7ddf67ba7745e1696126283a3d
charlinedes <charline.deswaef@gmail.com>
2024-12-11 16:30:07 +0100

    Qui sommes-nous

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qui sommes-nous</title>
+
+</head>
+<body>
+
+    <h1>BACK2PIXELS C’EST QUOI ?</h1>
+    <p>Back2pixels, la plateforme dédiée aux passionnés de jeux
+         vidéo ! Nous avons créé un espace où tout le monde 
+         peut facilement échanger ses jeux, consoles, et 
+         accessoires, tout cela sans se ruiner. 
+         Notre mission est simple : offrir aux gamers un moyen
+          pratique, sûr et économique de découvrir de nouveaux
+           jeux et de faire évoluer leur collection.</p>
+
+        <p>Nous croyons au partage et à la communauté. 
+        C’est pourquoi nous mettons en avant un système 
+        d’échange sécurisé pour vous permettre de trouver 
+        exactement ce que vous cherchez. Que vous soyez 
+        amateur de rétrogaming ou adepte des dernières 
+        nouveautés, notre plateforme est là pour connecter 
+        les joueurs et favoriser les échanges.</p>
+
+        <img src="Pers1.png" alt="Pers1" width="270" height="360">
+
+
+        <h1>NOTRE MISSION</h1>
+        <p>Chez Back2Pixels, notre mission est de rendre le monde
+             du gaming accessible à tous en créant un espace où
+              les passionnés peuvent échanger facilement leurs
+               jeux, consoles et accessoires. Nous voulons offrir
+                une alternative simple et économique pour 
+                renouveler ses jeux sans les contraintes d’achat
+                 et sans accumuler des objets inutilisés.</p>
+
+            <p>Nous croyons qu’un jeu doit être partagé, qu’une 
+            console doit vivre au-delà de son premier 
+            propriétaire, et que chaque accessoire mérite une 
+            seconde chance. Notre plateforme favorise des 
+            échanges responsables et soutient une communauté où
+             chaque transaction est une opportunité de faire 
+             découvrir de nouveaux univers et de connecter les
+              joueurs entre eux.</p>
+
+              <img src="Pers2.png" alt="Pers2" width="270" height="360">
+
+              <h1>NOTRE HISTOIRE</h1>
+              <p>L’aventure de Back2Pixels a commencé avec une
+                 simple idée : permettre aux joueurs de découvrir
+                  de nouveaux jeux sans se ruiner et de donner une
+                   seconde vie aux consoles et accessoires qui 
+                   dorment souvent dans nos placards. En tant que
+                    gamers passionnés, nous avons constaté combien
+                     il peut être coûteux de toujours acheter les
+                      derniers jeux et consoles, sans parler du 
+                      manque d’espace pour tout conserver !</p>
+
+                <p>Ce constat nous a inspirés à créer une plateforme
+                 qui facilite l’échange entre joueurs, offrant 
+                 une alternative simple et conviviale à l’achat
+                  traditionnel. Dès le départ, notre objectif a 
+                  été de bâtir une communauté de gamers qui 
+                  partagent notre passion et qui, comme nous, 
+                  voient dans chaque échange une opportunité 
+                  de découverte et de connexion.
+                
+                <p>Rejoignez-nous et faites partie de cette 
+                aventure : chaque échange est une nouvelle 
+                histoire !</p>
+
+                <img src="Pers3.png" alt="Pers3" width="270" height="360">
+
+                <h1>CONFIANCE ET SÉCURITÉ</h1>
+
+                <p>Nous savons que la sécurité et la confiance 
+                    sont essentielles pour une expérience 
+                    d’échange réussie. C’est pourquoi nous a
+                    vons mis en place des mesures solides pour 
+                    garantir que chaque transaction sur notre 
+                    plateforme se déroule dans les meilleures 
+                    conditions possibles.
+
+                    En cas de problème, notre service client est
+                     là pour vous accompagner et répondre à 
+                     toutes vos questions. Nous croyons fermement
+                      qu’un environnement sécurisé et de 
+                      confiance est la clé pour créer une 
+                      communauté de gamers soudée et respectueuse.
+                    
+                    Back2Pixels, vous pouvez échanger en toute
+                     sérénité et concentrer votre énergie sur ce
+                      qui compte vraiment : le plaisir de 
+                      découvrir de nouveaux jeux et de faire des
+                       rencontres dans un environnement sûr.</p>
+
+                       <img src="Pers4.png" alt="Pers4" width="270" height="360">
+
+
+</body>
+</html>
