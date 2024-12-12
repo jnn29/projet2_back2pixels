908d4f7387df37a16bc732f3a2c29cd239ab03cd
charlinedes <charline.deswaef@gmail.com>
2024-12-12 11:40:09 +0100

    update navbar

--- inscription.php
+++ inscription.php
@@ -4,42 +4,40 @@
 get_header();?>
 
 <body>
-<nav class="navbar navbar-expand-lg">
-<nav class="navbar" style="background-color: #7058A8;">
+    <nav class="navbar navbar-expand-lg" style="background-color: #7058A8;">
     <div class="container">
+      
         <a class="navbar-brand" href="index.php">
-            <img src="<?php echo get_template_directory_uri(); ?>/Fichier 4.svg" 
-                alt="Logo" width="60" height="48"></a>
+        <img src="<?php echo get_template_directory_uri(); ?>/Fichier 4.svg" 
+            alt="Logo" width="60" height="48">
+        </a>
 
+        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-            data-bs-target="#menu">
-               <span class="navbar-toggler-icon"></span>
+            data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
         </button>
 
     <div class="collapse navbar-collapse" id="menu">
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(64); ?>">Jeux vidéo</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(64); ?>">Jeux vidéo</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(68); ?>">Consoles</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(68); ?>">Consoles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(); ?>">Qui sommes-nous</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(); ?>">Qui sommes-nous</a>
             </li>
-            </ul>
+        </ul>
 
     <div class="ms-auto">
-            <button class="custom-button">S'inscrire/se connecter</button>
-            <button class="custom-button">Créer une annonce</button>
+        <button class="custom-button">S'inscrire/se connecter</button>
+        <button class="custom-button">Créer une annonce</button>
     </div>
     </div>
     </div>
-</nav>
-</nav>
+    </nav>
 
     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class="text-right">
--- mentionlegales.php
+++ mentionlegales.php
@@ -4,30 +4,30 @@
 get_header();?>
 
 <body>
-<nav class="navbar navbar-expand-lg">
-<nav class="navbar" style="background-color: #7058A8;">
+    <nav class="navbar navbar-expand-lg" style="background-color: #7058A8;">
     <div class="container">
+      
         <a class="navbar-brand" href="index.php">
-            <img src="<?php echo get_template_directory_uri(); ?>/Fichier 4.svg"
-                alt="Logo" width="60" height="48"></a>
+        <img src="<?php echo get_template_directory_uri(); ?>/Fichier 4.svg" 
+            alt="Logo" width="60" height="48">
+        </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+            data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
 
     <div class="collapse navbar-collapse" id="menu">
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(64); ?>">Jeux vidéo</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(64); ?>">Jeux vidéo</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(68); ?>">Consoles</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(68); ?>">Consoles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" 
-                    href="<?php echo get_permalink(); ?>">Qui sommes-nous</a>
+                <a class="nav-link active" aria-current="page" href="<?php echo get_permalink(); ?>">Qui sommes-nous</a>
             </li>
         </ul>
 
@@ -37,8 +37,7 @@ get_header();?>
     </div>
     </div>
     </div>
-</nav>
-</nav>
+    </nav>
 
 
     <div class="container">
