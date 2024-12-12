54f9494e464b5eaab2388193469caafd1357083c
charlinedes <charline.deswaef@gmail.com>
2024-12-12 11:45:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -39,27 +39,16 @@ get_header();?>
      </div>
      </nav>
 
-     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-     <div class="text-right">
-          <?php
-          echo "<h1>Connecte-toi !</h1>";
-          ?>
-            
-          <form action="welcome.php" method="post">
-     <div class="mb-3">
-          <label for="username" class="form-label">Nom d'utilisateur ou email</label>
-               <input type="text" class="form-control" id="username" name="username" required>
-     </div>
+     <div class="container">
+        <h1>MENTIONS LÉGALES</h1></div>
+        
+    <div class="container">Back2Pixels SRL</div>
 
-     <div class="mb-3">
-          <label for="password" class="form-label">Mot de passe</label>
-               <input type="password" class="form-control" id="password" name="password" required>
-     </div>
-          <button class="custom-button">Se connecter</button>
-          </form>
-     </div>
-     </div>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="container">Rue de la Poste 11, 1030 Schaerbeek</div>
+
+    <div class="container">E-mail: contact@back2pixels.com</div>
+
+    <div class="container">Téléphone: 0486/17.49.55</div><br>
 </body>
 
 <?php get_footer(); ?>
