d57e7453ea1c658bdaf25b1ebcd41176b724e622
charlinedes <charline.deswaef@gmail.com>
2024-12-16 15:16:40 +0100

    créer une annonce + update

--- profil.php
+++ profil.php
@@ -55,11 +55,11 @@ get_header();?>
 
         <div class="col-md-6">   
                 <h1>NARUTO_LPB</h1>   
-                <hr style="border-top: 4px solid #000; margin: 15px 0;">  
+            <hr style="border-top: 4px solid #000; margin: 15px 0;">  
                 <p><b>Bio</b> : J’ai 27 ans et je vis à Bruxelles. 
-                Je suis collectionneur de jeux vidéo et consoles rétro (Playstaion, Nintendo DS, Gameboy, PSP,...). 
-                Je veux pouvoir agrandir ma collection sans devoir dépenser d’énormes sommes. 
-                Et, si c’est possible, faire de nouvelles rencontres et partager ma passion.
+                    Je suis collectionneur de jeux vidéo et consoles rétro (Playstaion, Nintendo DS, Gameboy, PSP,...). 
+                    Je veux pouvoir agrandir ma collection sans devoir dépenser d’énormes sommes. 
+                    Et, si c’est possible, faire de nouvelles rencontres et partager ma passion.
                 </p>
     
             <div class="d-flex justify-content-end mt-3">
